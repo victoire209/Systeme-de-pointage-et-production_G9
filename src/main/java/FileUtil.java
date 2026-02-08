17fdd216d35f77b3d9b2aa7272443854e891d5b2
victoire209 <victoiremkl1@gmail.com>
2026-02-08 21:42:56 +0200

    voici le bon dossier dieudo

---
+++ src/main/java/FileUtil.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+
+//la class qui cree un ficier rapport
+    class fichierRapport {
+        public static void fichierRapports() {  // Méthode statique
+            String reportPath = "D:/vic_java/Systeme de pointage et production_G9/data";
+            File fichier = new File(reportPath);
+
+            try{
+                if (fichier.createNewFile()) {
+                    System.out.println("Fichier créé avec succès");
+                } else {
+                    System.out.println("Fichier existe déjà");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            String productionPath = "D:/vic_java/Systeme de pointage et production_G9/data";
+
+            try(
+                    Scanner lect = new Scanner(System.in);
+                    BufferedWriter bw = new BufferedWriter(new FileWriter(productionPath, true)))
+
+            {
+
+                System.out.println("Veuillez entrer les données de la production : ");
+
+                String infoProduction = lect.nextLine();
+                bw.write(infoProduction);
+                bw.newLine();
+
+            } catch(IOException e)
+
+            {
+                System.out.println("erreur lors de l'ecriture du fichier!");
+                e.printStackTrace();
+            }
+        }
+    }
+    //la class qui ecrit la production et l'enregistre dans un fichier
+    class ecritureProduction {
+        public static void ecritureProductions() {  // Méthode statique
+            String productionPath = "D:/vic_java/Systeme de pointage et production_G9/data/production.csv";
+
+            try (Scanner lect = new Scanner(System.in);
+                 BufferedWriter bw = new BufferedWriter(new FileWriter(productionPath, true))) {
+
+                System.out.println("Veuillez entrer les données de la production : ");
+                String infoProduction = lect.nextLine();
+                bw.write(infoProduction);
+                bw.newLine();
+
+            } catch (IOException e) {
+                System.out.println("Erreur lors de l'écriture du fichier!");
+                e.printStackTrace();
+            }
+        }
+    }
+//la class qui ecrit les operateur dans un fichier
+class ecritureOperator {
+    public static void ecrireOperateurs() {  // Méthode statique
+        String operatorsPath = "D:/vic_java/Systeme de pointage et production_G9/data/operators.txt";
+
+        try (Scanner lect = new Scanner(System.in);
+             BufferedWriter bw = new BufferedWriter(new FileWriter(operatorsPath, true))) {
+
+            System.out.println("Veuillez entrer les identités de l'opérateur : ");
+            String infoOperators = lect.nextLine();
+            bw.write(infoOperators);
+            bw.newLine();
+
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'écriture du fichier!");
+            e.printStackTrace();
+        }
+    }
+}
